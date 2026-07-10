06fc8da79a02ac5d952f70f58651dc3933fd343e
Paul Burca <Paul.Burca@softwire.com>
2026-07-10 14:58:15 +0300

    bishop rules

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
+import GameSettings from '../gameSettings';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,39 @@ export default class Bishop extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves:Array<Square> = new Array(0);
+        const currentSquare = board.findPiece(this);
+        
+        let x:number, y:number;
+
+        x = currentSquare.row + 1;
+        y = currentSquare.col + 1;
+        while (x < GameSettings.BOARD_SIZE && y < GameSettings.BOARD_SIZE) { 
+            availableMoves.push(Square.at(x, y));
+            x++;
+            y++;
+        }
+        x = currentSquare.row - 1;
+        y = currentSquare.col + 1;
+        while (x >= 0 && y < GameSettings.BOARD_SIZE) { 
+            availableMoves.push(Square.at(x, y));
+            x--;
+            y++;
+        }
+        x = currentSquare.row - 1;
+        y = currentSquare.col - 1;
+        while (x >= 0 && y >= 0) { 
+            availableMoves.push(Square.at(x, y));
+            x--;
+            y--;
+        }
+        x = currentSquare.row + 1;
+        y = currentSquare.col - 1;
+        while (x < GameSettings.BOARD_SIZE && y >= 0) { 
+            availableMoves.push(Square.at(x, y));
+            x++;
+            y--;
+        }
+        return availableMoves;
     }
 }
