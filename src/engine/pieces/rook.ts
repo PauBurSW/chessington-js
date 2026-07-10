91a431a23d4d5826539ddc6e33d387ad9b0289cf
Paul Burca <Paul.Burca@softwire.com>
2026-07-10 14:44:47 +0300

    rook rules

--- src/engine/pieces/rook.ts
+++ src/engine/pieces/rook.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class Rook extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,18 @@ export default class Rook extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves:Array<Square> = new Array(0);
+        const currentSquare = board.findPiece(this);
+        
+        for (let i = 0; i < GameSettings.BOARD_SIZE; i++) { 
+            if (i != currentSquare.col) {
+                availableMoves.push(Square.at(currentSquare.row, i));
+            }
+            if (i != currentSquare.row) {
+                availableMoves.push(Square.at(i, currentSquare.col));
+            }
+        }
+
+        return availableMoves;
     }
 }
